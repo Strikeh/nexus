a44aa5d37a06e54f66ec6c4972222bd3e44d3f7e
Christophe <christophe.verheyen@trivec.be>
2023-02-19 15:38:58 +0100

    Blockchain page

---
+++ src/app/blockchain/blockchain.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  templateUrl: './blockchain.component.html',
+  styleUrls: ['./blockchain.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BlockchainComponent {}
